7734947269daefc72b1c5e64a2d4970c6f44b83d
Amairi <selsabilamairi@gmail.com>
2025-05-12 11:43:21 +0200

    Activité 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1>Welcome Home !</h1>;
+    return <h1>Welcome Home!</h1>;
   }
-  
+
   export default Home;
-  
